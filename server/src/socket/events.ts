ccf8247e585a2078fabd663c04abc004d53881e9
rqphy <raphael.ferreira@hetic.net>
2025-12-22 21:59:55 +0100

    add events name

---
+++ server/src/socket/events.ts
@@ -0,0 +1,17 @@
+export const SOCKET_EVENTS = {
+	CONNECTION: "connection",
+	DISCONNECTION: "disconnect",
+
+	CREATE_ROOM: "create_room",
+	JOIN_ROOM: "join_room",
+	LEAVE_ROOM: "leave_room",
+	PLAYER_JOINED: "player_joined",
+	PLAYER_LEFT: "player_left",
+
+	START_GAME: "start_game",
+	GAME_STARTED: "game_started",
+	END_GAME: "end_game",
+	NEW_ROUND: "new_round",
+	END_ROUND: "end_round",
+	SUBMIT_ANSWER: "submit_answer",
+}
