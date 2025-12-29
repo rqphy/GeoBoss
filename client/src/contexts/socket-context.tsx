e520db1462b7319ede19446586323f17971bbdc5
rqphy <raphael.ferreira@hetic.net>
2025-12-29 13:52:02 +0100

    work on client socket manager

---
+++ client/src/contexts/socket-context.tsx
@@ -0,0 +1,142 @@
+import { createContext, useContext, useEffect, useState } from "react"
+import { Socket, io } from "socket.io-client"
+import { SOCKET_EVENTS } from "../socket/events.ts"
+import type { Player } from "../types/game.ts"
+
+interface SocketProviderMethods {
+    createRoom:(playerName: string) => void
+    joinRoom:(playerName: string, roomId: string) => void
+    leaveRoom:() => void
+
+    startGame:() => void
+    submitAnswer:(answer: string) => void   
+}
+
+interface SocketState {
+    isConnected: boolean
+    roomId: string
+    playersList: Player[]
+    isGameStarted: boolean
+    currentRound: number
+}
+
+interface SocketProviderProps {
+    children: React.ReactNode
+}
+
+interface SocketContextValue {
+    socket: Socket
+    methods: SocketProviderMethods
+    state: SocketState
+}
+
+const socketContext = createContext<SocketContextValue | null>(null)
+const serverAddress = "http://localhost:3000"
+const socket: Socket = io(serverAddress, {
+    withCredentials: true,
+    transports: ["websocket"],
+})
+
+export function SocketProvider({ children }: SocketProviderProps) {
+    const [playersList, setPlayersList] = useState<Player[]>([])
+    const [isConnected, setIsConnected] = useState<boolean>(false)
+    const [roomId, setRoomId] = useState<string>("")
+    const [isGameStarted, setIsGameStarted] = useState<boolean>(false)
+    const [currentRound, setCurrentRound] = useState<number>(0)
+
+    useEffect(() => {
+        socket.on(SOCKET_EVENTS.CONNECTION, () => {
+            setIsConnected(true)
+        })
+        socket.on(SOCKET_EVENTS.DISCONNECTION, () => {
+            setIsConnected(false)
+        })
+
+        socket.on(SOCKET_EVENTS.PLAYER_JOINED, (player: Player) => {
+            setPlayersList((prevPlayers) => [...prevPlayers, player])
+        })
+
+        socket.on(SOCKET_EVENTS.PLAYER_LEFT, (playerId: string) => {
+            setPlayersList((prevPlayers) => prevPlayers.filter((p) => p.id !== playerId))
+        })
+
+        socket.on(SOCKET_EVENTS.ROOM_CREATED, (roomId: string) => {
+            setRoomId(roomId)
+        })
+
+        socket.on(SOCKET_EVENTS.GAME_STARTED, () => {
+            setIsGameStarted(true)
+        })
+
+        socket.on(SOCKET_EVENTS.END_GAME, () => {
+            setIsGameStarted(false)
+        })
+
+        socket.on(SOCKET_EVENTS.NEW_ROUND, ({round, country, timeLimit}: {round: number, country: string, timeLimit: number}) => {
+            setCurrentRound(round)
+        })
+
+        socket.on(SOCKET_EVENTS.END_ROUND, ({correctAnswer, scores}: {correctAnswer: string, scores: {id: string, name: string, score: number}[]}) => {
+            // TODO: update players list
+        })
+
+        socket.on(SOCKET_EVENTS.GOOD_ANSWER, ({playerId, score}: {playerId: string, score: number}) => {
+            setPlayersList((prevPlayers) =>
+                prevPlayers.map((p) =>
+                    p.id === playerId ? { ...p, score: score } : p
+                )
+            )
+        })
+
+        socket.on(SOCKET_EVENTS.BAD_ANSWER, ({playerId}: {playerId: string}) => {
+            // TODO: if playerId correspond -> show it's a bad answer + bg
+        })
+
+        return () => {
+            socket.off(SOCKET_EVENTS.CONNECTION)
+            socket.off(SOCKET_EVENTS.DISCONNECTION)
+        }
+
+    }, [])
+    
+
+    const methods: SocketProviderMethods = {
+        createRoom(playerName: string) {
+            socket.emit("createRoom", { playerName })
+        },
+        joinRoom(playerName: string, roomId: string) {
+            socket.emit("joinRoom", { playerName, roomId })
+        },
+        leaveRoom() {
+            socket.emit("leaveRoom")
+        },
+        startGame() {
+            socket.emit("startGame")
+        },
+        submitAnswer(answer: string) {
+            socket.emit("submitAnswer", { answer })
+        },
+    }
+    
+    const state: SocketState = {
+        isConnected,
+        roomId,
+        playersList,
+        isGameStarted,
+        currentRound,
+    }
+    
+    return (
+        <socketContext.Provider value={{ socket, methods, state }}>
+            {children}
+        </socketContext.Provider>
+    )
+}
+
+export function useSocket() {
+    const context = useContext(socketContext)
+    if (!context) {
+        throw new Error("useSocket must be used within a SocketProvider")
+    }
+    return context
+}
---
+++ client/src/socket/events.ts
@@ -0,0 +1,22 @@
+export const SOCKET_EVENTS = {
+	CONNECTION: "connection",
+	DISCONNECTION: "disconnect",
+
+	CREATE_ROOM: "create_room",
+	JOIN_ROOM: "join_room",
+	LEAVE_ROOM: "leave_room",
+	PLAYER_JOINED: "player_joined",
+	PLAYER_LEFT: "player_left",
+	ROOM_CREATED: "room_created",
+
+	START_GAME: "start_game",
+	GAME_STARTED: "game_started",
+	END_GAME: "end_game",
+	NEW_ROUND: "new_round",
+	END_ROUND: "end_round",
+	SUBMIT_ANSWER: "submit_answer",
+	GOOD_ANSWER: "good_answer",
+	BAD_ANSWER: "bad_answer",
+
+	ERROR: "error",
+}
