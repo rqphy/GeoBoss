a6d1318aa9891c97e2720c741afc1c5ab6e3e24e
rqphy <raphael.ferreira@hetic.net>
2026-02-16 12:06:52 +0100

    bug/fix cors prod issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,12 +15,7 @@ const io = new Server(httpServer, {
 	},
 })
 
-app.use(
-	cors({
-		origin: config.nodeEnv === "development" ? "*" : config.clientUrl,
-		credentials: true,
-	})
-)
+app.use(cors())
 app.use(express.json())
 
 app.use("/api", apiRoutes)
